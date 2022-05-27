9c639727758c186c8a6996bc33a0fb608c914477
Law3333 <100847273+Law3333@users.noreply.github.com>
2022-05-27 21:40:57 +0800

    crawler_info.py

--- crawler_info.py
+++ crawler_info.py
@@ -2,29 +2,32 @@ import bs4
 import urllib.request as req
 from urllib.parse import quote
 
-# input 使用 UrlDecode 編碼
-
 
 class get_basic_info:
     def __init__(self, keyword):
-        self.keyword_urldecode = quote(keyword)
-        self.url_front = "https://libholding.ntut.edu.tw/booksearch.do?searchtype=simplesearch&search_field=FullText&search_input="
-        self.url_back = "&searchsymbol=hyLibCore.webpac.search.common_symbol&execodehidden=true&execode=&ebook="
-        self.url = self.url_front + self.keyword_urldecode + self.url_back
 
-    def executive(self):
+        # input 使用 UrlDecode 編碼
+        self.keyword_urldecode = quote(keyword)
+
+        # Headers info
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
+        }
+
+    def get_isbn(self):
+
+        url_front = "https://libholding.ntut.edu.tw/booksearch.do?searchtype=simplesearch&search_field=FullText&search_input="
+        url_back = "&searchsymbol=hyLibCore.webpac.search.common_symbol&execodehidden=true&execode=&ebook="
+        url = url_front + self.keyword_urldecode + url_back
 
         # let program like a people
         # 建立一個 Request 物件，附加 Headers 的資訊
-        requert = req.Request(self.url, headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36"
-        })
+        self.requert = req.Request(url, headers=self.headers)
 
-        with req.urlopen(requert) as response:
+        with req.urlopen(self.requert) as response:
             data = response.read().decode("utf-8")
-        # print(data)
 
-        # ---解析原始碼---
+        # ---解析原始碼(html)---
 
         root = bs4.BeautifulSoup(data, "html.parser")
         # print(root.li.string)  # 作者：韓國瑜
@@ -37,5 +40,7 @@ class get_basic_info:
         #     if auther.a != None:  # 如果標題包含 li 標籤 (沒有被刪除)，印出來
         #         print(auther.a)
 
+    # def get_info(self):
 
-get_basic_info("跟著月亮").executive()
+
+get_basic_info("跟著月亮").get_isbn()
